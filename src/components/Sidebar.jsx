63ebacde2486abfe7c2d1de0e32738be4320285c
Naresh Barfa <nareshbarfa007@gmail.com>
2025-01-19 22:55:33 +0530

    added portfolio link in cv resume

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
         <!-- href="YOUR_RESUME_LINK_HERE": Replace this with the direct link to your resume file. -->
         <!-- download="YourName_Resume.pdf": Specifies the name of the file that will be downloaded.  --> */}
         <a
-          href="https://drive.google.com/file/d/1rP8KC1v18i9A2NKtQ-F8hO3fWj5Jo3Wp/view?usp=sharing"
+          href="https://drive.google.com/file/d/1mjntG2dlVgMx165B8BuCh4c7jD2mMVQs/view?usp=sharing"
           target="_blank"
           className="flex gap-8 items-center text-xs bg-green-500 text-white px-3 py-2 mt-6 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-300"
         >
@@ -84,7 +84,7 @@ const Sidebar = () => {
         </a>
 
         <a
-          href="https://drive.google.com/file/d/12K3BY70-QWaoaAG8Anx90u6HTv9luddy/view?usp=sharing"
+          href="https://drive.google.com/file/d/1Ke5BdR3ZANa1yRQKsA3SO5I2UdREVtTB/view?usp=sharing"
           target="_blank"
           className="flex gap-8 items-center text-xs bg-green-500 text-white px-3 py-2 mt-6 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-300"
         >
